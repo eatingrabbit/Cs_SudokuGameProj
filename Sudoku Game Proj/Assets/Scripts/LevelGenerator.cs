68ebd6a4123e7ac7edc21b281211bda507f12c42
eatingrabbit <lhy100494@gmail.com>
2021-10-31 23:53:18 +0900

    ~9x9 sprites & text

---
+++ Sudoku Game Proj/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    
+    int[,] ary = new int[9, 9] {{ 1,2,3,4,5,6,7,8,9 },
+                                { 4,5,6,7,8,9,1,2,3 },
+                                { 7,8,9,1,2,3,4,5,6 },
+                                { 2,3,4,5,6,7,8,9,1 },
+                                { 5,6,7,8,9,1,2,3,4 },
+                                { 8,9,1,2,3,4,5,6,7 },
+                                { 3,4,5,6,7,8,9,1,2 },
+                                { 6,7,8,9,1,2,3,4,5 },
+                                { 9,1,2,3,4,5,6,7,8 }};
+
+    public static LevelGenerator instance;
+
+    void Awake() { instance = this; }
+
+    void Start()
+    {
+
+
+    }
+
+}
